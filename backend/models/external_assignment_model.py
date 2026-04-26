106dde72564d1e39c5058ab18360c52c3e69fe8c
Aungaing <mintita.kul@gmail.com>
2026-04-26 08:44:14 +0700

    implement model and repository layers

---
+++ backend/models/external_assignment_model.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, String, DateTime, Text, UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID, JSONB
+from sqlalchemy.sql import func
+import uuid
+from database import Base
+
+class ExternalAssignment(Base):
+    __tablename__ = "external_assignments"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    user_id = Column(UUID(as_uuid=True), nullable=False)
+    source_name = Column(String(50), nullable=True)
+    external_course_name = Column(Text)
+    external_course_url = Column(Text)
+    title = Column(Text)
+    external_link = Column(Text)
+    submission_status = Column(Text)
+    grading_status = Column(Text)
+    due_date = Column(DateTime)
+    time_remaining = Column(Text)
+    last_modified = Column(Text)
+    file_submission = Column(Text)
+    raw_data = Column(JSONB)
+    created_at = Column(DateTime, server_default=func.now())
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "source_name", "external_link", name="unique_ext_assign"),
+    )
