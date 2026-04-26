5fcb747ff1caec98e52ce26770e66dd3f35b3cde
HusnaRod <husna.rot@dome.tu.ac.th>
2026-04-26 11:29:28 +0700

    finish student mycourses page

--- frontend/student/student-mycourses.js
+++ frontend/student/student-mycourses.js
@@ -1,3 +1,24 @@
+// sidebar
+fetch('../components/student-sidebar/sidebar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('sidebar-placeholder').innerHTML = data;
+
+        const script = document.createElement("script");
+        script.src = "../components/student-sidebar/sidebar.js";
+        document.body.appendChild(script);
+    });
+
+// navbar
+fetch('../components/student-navbar/student-navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+
+        const script = document.createElement("script");
+        script.src = "../components/student-navbar/student-navbar.js";
+        document.body.appendChild(script);
+    });
 const courses = [
   { code: "CS222", name: "OPERATING SYSTEMS",                           bg: "#E8926A" },
   { code: "CS232", name: "INTRODUCTION TO CLOUD\nCOMPUTING TECHNOLOGY", bg: "#3BCFCF" },
@@ -13,8 +34,9 @@ const docSVG = `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stro
 </svg>`;
 
 const gearSVG = `<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2.2" stroke-linecap="round" stroke-linejoin="round">
-  <circle cx="12" cy="12" r="3"/>
-  <path d="M19.4 15a1.65 1.65 0 00.33 1.82l.06.06a2 2 0 010 2.83 2 2 0 01-2.83 0l-.06-.06a1.65 1.65 0 00-1.82-.33 1.65 1.65 0 00-1 1.51V21a2 2 0 01-4 0v-.09A1.65 1.65 0 009 19.4a1.65 1.65 0 00-1.82.33l-.06.06a2 2 0 01-2.83-2.83l.06-.06A1.65 1.65 0 004.68 15a1.65 1.65 0 00-1.51-1H3a2 2 0 010-4h.09A1.65 1.65 0 004.6 9a1.65 1.65 0 00-.33-1.82l-.06-.06a2 2 0 012.83-2.83l.06.06A1.65 1.65 0 009 4.68a1.65 1.65 0 001-1.51V3a2 2 0 014 0v.09a1.65 1.65 0 001 1.51 1.65 1.65 0 001.82-.33l.06-.06a2 2 0 012.83 2.83l-.06.06A1.65 1.65 0 0019.4 9a1.65 1.65 0 001.51 1H21a2 2 0 010 4h-.09a1.65 1.65 0 00-1.51 1z"/>
+  <path d="M22 8.5c0 2.5-1.5 4.5-4 5.5H6c-2.5-1-4-3-4-5.5S3.5 4 6 3h12c2.5 1 4 3 4 5.5z"/>
+<path d="M6 14v4"/>
+<path d="M10 14v4"/>
 </svg>`;
 
 const grid = document.getElementById("coursesGrid");
